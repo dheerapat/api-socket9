abc4912a887b59a2a18c570a183ba6531a76e023
Dheerapat Tookkane <dheerapat.t@kkumail.com>
2023-03-20 20:43:22 +0700

    fix put and delete path

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -45,15 +45,15 @@ public class prodController : ControllerBase
         return Ok(product);
     }
 
-    [HttpPut("{id}/updatecategory")]
-    public IActionResult UpdateCategory(int id, int categoryId)
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, ProductRequestViewModel Product)
     {
         var productToUpdate = _service.GetById(id);
 
         if(productToUpdate is not null)
         {
-            _service.UpdateCategory(id, categoryId);
-            return NoContent();    
+            _service.Update(id, Product);
+            return NoContent();
         }
         else
         {
@@ -68,7 +68,7 @@ public class prodController : ControllerBase
 
         if(product is not null)
         {
-            _service.DeleteById(id);
+            _service.Delete(id);
             return Ok();
         }
         else
--- Service/ProductService.cs
+++ Service/ProductService.cs
@@ -27,20 +27,31 @@ public class prodService
         using(var uow = _uowProvider.CreateUnitOfWork())
         {
             var repo = uow.GetRepository<Product>();
-            // normally we query with condition isDelete == true
-            var result = repo.Filters(x => x.id > 0).ToList();
+            // normally we query with condition isDelete == false
+            var result = repo.Filters(x => x.is_deleted == false).ToList();
             return _mapper.Map<List<Product>>(result);
         }
     }
 
     public Product? GetById(int id)
     {
-        return _context.Products
-            .Include(p => p.Category)
-            .AsNoTracking()
-            .SingleOrDefault(p => p.id ==id);
+        using (var uow = _uowProvider.CreateUnitOfWork())
+        {
+            var repo = uow.GetRepository<Product>();
+            var product = repo.Filters(x => x.id == id && x.is_deleted == false).FirstOrDefault();
+            if (product == null)
+            {
+                throw new Exception("Product not found");
+            }
+            return _mapper.Map<Product>(product);
+        }
+        // return _context.Products
+        //     .Include(p => p.Category)
+        //     .AsNoTracking()
+        //     .SingleOrDefault(p => p.id ==id);
     }
 
+    // normally return a response view model
     public Product? Create(ProductRequestViewModel newProduct)
     {
         using (var uow = _uowProvider.CreateUnitOfWork())
@@ -53,28 +64,57 @@ public class prodService
         }
     }
 
-    public void UpdateCategory(int productId, int categoryId)
+    public bool Update(int id, ProductRequestViewModel Product)
     {
-        var productToUpdate = _context.Products.Find(productId);
-        var categoryToUpdate = _context.Categories.Find(categoryId);
-
-        if (productToUpdate is null || categoryToUpdate is null)
+        using (var uow = _uowProvider.CreateUnitOfWork())
         {
-            throw new InvalidOperationException("Product or category does not exist");
+            var repo = uow.GetRepository<Product>();
+            var existingProduct = repo.GetActive(id);
+
+            if (existingProduct == null)
+            {
+                throw new Exception("Product not found");
+            }
+            
+            existingProduct = _mapper.Map<ProductRequestViewModel, Product>(Product, existingProduct);
+            uow.SaveChanges();
+            return true;
         }
+        // var productToUpdate = _context.Products.Find(productId);
+        // var categoryToUpdate = _context.Categories.Find(categoryId);
 
-        productToUpdate.Category = categoryToUpdate;
+        // if (productToUpdate is null || categoryToUpdate is null)
+        // {
+        //     throw new InvalidOperationException("Product or category does not exist");
+        // }
 
-        _context.SaveChanges();
+        // productToUpdate.Category = categoryToUpdate;
+
+        // _context.SaveChanges();
     }
 
-    public void DeleteById(int id)
+    public bool Delete(int id)
     {
-        var productToDelete = _context.Products.Find(id);
-        if (productToDelete is not null)
+        using (var uow = _uowProvider.CreateUnitOfWork())
         {
-            _context.Products.Remove(productToDelete);
-            _context.SaveChanges();
+            var repo = uow.GetRepository<Product>();
+            var existingProduct = repo.GetActive(id);
+
+            if (existingProduct == null)
+            {
+                throw new Exception("Product not found");
+            }
+
+            existingProduct.is_deleted = true;
+            uow.SaveChanges();
+
+            return true;
         }
+        // var productToDelete = _context.Products.Find(id);
+        // if (productToDelete is not null)
+        // {
+        //     _context.Products.Remove(productToDelete);
+        //     _context.SaveChanges();
+        // }
     }
 }
